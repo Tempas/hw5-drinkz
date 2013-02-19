833b562cdb30c5a3f1c5a85636eea88419cea69b
Jon Bauer <bauerjon@13-69-95.client.wireless.msu.edu>
2013-02-19 14:29:38 -0500

    HW3 Part3 a,b,c finished no tests

--- drinkz/recipes.py
+++ drinkz/recipes.py
@@ -1,3 +1,5 @@
+import db
+
 class Recipe(object):
     _recipeName = ""
     _myIngredients = set()
@@ -5,9 +7,38 @@ class Recipe(object):
         _recipeName = name
         for ingredient in ingredientList:
             _myIngredients.add(ingredient)
-        
 
-    
+    def need_ingredients():
+        myList = list()
+        for currentIngredient in _myIngredients:
+            setOfCertainType = check_inventory_for_type(currenIngredient[0])
+            for value in setOfCertainType:
+                totalAmountOfCertainType = get_liqour_amount(value[0],value[1])
+                ingredientAmount = convert_to_ml(currentIngredient[1])
+                if( ingredientAmount > totalAmountOfCertainType ):
+                    myList.append((currentIngredient[0],ingredientAmount - totalAmountOfCertainType))
+        return myList
+                    
+                
+
+    def convert_to_ml(amount):
+        amounts = amount.split(" ")
+        total = 0
+        if amounts[1] == "oz":
+            total += float(amounts[0])*29.5735
+        elif amounts[1] == "ml":
+            total += float(amounts[0])
+        elif amounts[1] == "gallon":
+            total += float(amounts[0])*3785.41178
+        return total
+
+    def check_inventory_for_type(liqourType):
+        mySet = set()
+        for m, l in db.get_liquor_inventory():
+            if liqourType == l:
+                mySet.add((m,l))
+
+        return mySet
             
             
         
