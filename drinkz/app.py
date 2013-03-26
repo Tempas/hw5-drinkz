2034b963b0ae5895697f29db8d06f74d774bbd0d
Jon Bauer <bauerjon@13-111-33.client.wireless.msu.edu>
2013-03-26 15:37:33 -0400

    HW4 Finished Step 6 before check

--- drinkz/app.py
+++ drinkz/app.py
@@ -15,7 +15,6 @@ dispatch = {
     '/inventoryList' : 'inventoryList',
     '/liqourTypes' : 'liqourTypes',
     '/convertToML' : 'formConvertToML',
-    '/recv' : 'recv',
     '/recvAmount' : 'recvAmount',
     '/rpc' : 'dispatch_rpc'
 }
@@ -42,14 +41,33 @@ class SimpleApp(object):
             
     def index(self, environ, start_response):
         data = """\
+<html>
+<head>
+<title>Home</title>
+<style type='text/css'>
+h1 {color:red;}
+body {
+font-size: 17px;
+}
+</style>
+<script>
+function myFunction()
+{
+alert("Hello! I am an alert box!");
+}
+</script>
+</script>
+<h1>Drinkz Home</h1>
 Visit:
 <a href='recipesList'>Recipes</a>,
 <a href='inventoryList'>Inventory</a>,
 <a href='liqourTypes'>Liqour Types</a>,
-<a href='convertToML'>Convert to ml</a>, or
-<a href='form'>a form...</a>
+<a href='convertToML'>Convert to ml</a>
 <p>
-<img src='/helmet'>
+<input type="button" onclick="myFunction()" value="Show alert box" />
+</head>
+<body>
+
 """
         start_response('200 OK', list(html_headers))
         return [data]
@@ -77,44 +95,20 @@ Visit:
         start_response('200 OK', list(html_headers))
         return [data]
 
-    def helmet(self, environ, start_response):
-        content_type = 'image/gif'
-        data = open('Spartan-helmet-Black-150-pxls.gif', 'rb').read()
-
-        start_response('200 OK', [('Content-type', content_type)])
-        return [data]
-
-    def form(self, environ, start_response):
-        data = form()
-
-        start_response('200 OK', list(html_headers))
-        return [data]
     
     def formConvertToML(self, environ, start_response):
         data = convertToML()
 
         start_response('200 OK', list(html_headers))
         return [data]
-   
-    def recv(self, environ, start_response):
-        formdata = environ['QUERY_STRING']
-        results = urlparse.parse_qs(formdata)
 
-        firstname = results['firstname'][0]
-        lastname = results['lastname'][0]
-
-        content_type = 'text/html'
-        data = "First name: %s; last name: %s. <p><a href='./'>return to index</a>" % (firstname, lastname)
-
-        start_response('200 OK', list(html_headers))
-        return [data]
     
     def recvAmount(self, environ, start_response):
         formdata = environ['QUERY_STRING']
         results = urlparse.parse_qs(formdata)
 
         amount = results['amount'][0]
-        print amount
+        
         amount = str(db.convert_to_ml(amount))
         
 
@@ -164,6 +158,19 @@ Visit:
         response = simplejson.dumps(response)
         return str(response)
 
+    def rpc_convert_units_to_ml(self,amount):
+        return str(db.convert_to_ml(amount))
+    def rpc_get_recipe_names(self):
+        recipeList = db.get_all_recipes()
+        nameList = list()
+        for recipe in recipeList:
+            nameList.append(recipe._recipeName)
+        return nameList
+    def rpc_get_liqour_inventory(self):
+        liqourInvList = list()
+        for (m,l) in db.get_liquor_inventory():
+            liqourInvList.append((m,l))
+        return liqourInvList
     def rpc_hello(self):
         return 'world!'
 
@@ -172,58 +179,99 @@ Visit:
         return int(a) + int(b)
 
     
-def form():
-    return """
-<form action='recv'>
-Your first name? <input type='text' name='firstname' size'20'>
-Your last name? <input type='text' name='lastname' size='20'>
-<input type='submit'>
-</form>
-<p><a href='/'>Home</a>
-"""
+
 
 def convertToML():
     return """
+<html>
+<head>
+<title>Convert to ml</title>
+<style type='text/css'>
+h1 {color:red;}
+body {
+font-size: 17px;
+}
+</style>
+<h1>Convert to ml</h1>
 <form action='recvAmount'>
 Enter amount(i.e. 11 gallon or 120 oz)<input type='text' name='amount' size'20'>
 <input type='submit'>
 </form>
 <p><a href='/'>Home</a>
+</head>
+<body>
 """
 
 
 def recipesList():
     recipeList = db.get_all_recipes()
-    recipeStringHTML = "<ul>"
+    recipeStringHTML = """\
+<html>
+<head>
+<title>Recipe List</title>
+<style type='text/css'>
+h1 {color:red;}
+body {
+font-size: 17px;
+}
+</style>
+<h1>Recipe List</h1><ul>"""
     for recipe in recipeList:
         if recipe.need_ingredients():
             val = "no"
         else:
            val = "yes"
         recipeStringHTML += ("<li>"+recipe._recipeName + " " + val +"<p>")
-    recipeStringHTML += ("</ul>"+"<p><a href='/'>Home</a>")
+    recipeStringHTML += ("</ul>"+"<p><a href='/'>Home</a>"+"""</head>
+<body>""")
 
     return recipeStringHTML
 
 def inventoryList():
-    inventoryStringHTML = "<ul>"
+    inventoryStringHTML = """\
+<html>
+<head>
+<title>Inventory List</title>
+<style type='text/css'>
+h1 {color:red;}
+body {
+font-size: 17px;
+}
+</style>
+<h1>Inventory List</h1><ul>"""
     for (m,l) in db.get_liquor_inventory():
         inventoryStringHTML+= ("<li>" + str(m)+ " " + str(l)+ " "  + str(db.get_liquor_amount(m,l))+" ml"+ "<p>")
-    inventoryStringHTML += ("</ul>"+"<p><a href='/'>Home</a>")
+    inventoryStringHTML += ("</ul>"+"<p><a href='/'>Home</a>"+"""</head>
+<body>""")
 
     return inventoryStringHTML
     
 def liqourTypesList():
-    liqourTypeStringHTML = "<ul>"
+    liqourTypeStringHTML = """\
+<html>
+<head>
+<title>Liqour Types List</title>
+<style type='text/css'>
+h1 {color:red;}
+body {
+font-size: 17px;
+}
+</style>
+<h1>Liqour Types List</h1><ul>"""
     for (m,l) in db.get_liquor_inventory():
         liqourTypeStringHTML += ("<li>" + str(m)+ " " + str(l) + "<p>")
-    liqourTypeStringHTML += ("</ul>"+"<p><a href='/'>Home</a>")
+    liqourTypeStringHTML += ("</ul>"+"<p><a href='/'>Home</a>"+"""</head>
+<body>""")
 
     return liqourTypeStringHTML
 
 def setUpWebServer():
     import random, socket
     port = random.randint(8000, 9999)
+    filename = 'database'
+
+
+    load_db(filename)
     
     app = SimpleApp()
     
@@ -232,20 +280,20 @@ def setUpWebServer():
     print "Try using a Web browser to go to http://%s:%d/" % \
           (socket.getfqdn(), port)
     httpd.serve_forever()
-    
-if __name__ == '__main__':
-    args = sys.argv
-    try:
-        filename = args[1]
-    except:
-        filename = 'myTest'
-
-    for line in fileinput.input(['../bin/'+filename], inplace=True):
-        line = line.replace("drinkz.", "")
-        sys.stdout.write(line)
-
-    load_db('../bin/'+filename)
-    setUpWebServer()
+##    
+##if __name__ == '__main__':
+##    args = sys.argv
+##    try:
+##        filename = args[1]
+##    except:
+##        filename = 'myTest'
+##
+##    for line in fileinput.input(['../bin/'+filename], inplace=True):
+##        line = line.replace("drinkz.", "")
+##        sys.stdout.write(line)
+##
+##    load_db('../bin/'+filename)
+##    setUpWebServer()
 
 
 
